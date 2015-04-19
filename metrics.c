678a02250e3cfcfed66f0c45a831695896aaf0c0
Laurence-Cullen <laurencecullen@live.co.uk>
2015-04-19 17:49:37 +0100

    Attempted to normalize metrics somewhat

--- metrics.c
+++ metrics.c
@@ -163,13 +163,20 @@ int main (int argc, char *argv[])
     
 
     //MAGMEAN MAGMIN MAGMAX MAGDIFF MAXMEAN MINMEAN 
-    printf("{ %lf %lf %lf %lf %lf %lf ",mag_mean, mag_min, mag_max, mag_diff, max_mean, min_mean);
+    printf("{ %lf %lf %lf %lf %lf %lf ",
+    mag_mean*SCAL1, mag_min*SCAL1, mag_max*SCAL1, mag_diff*SCAL1, max_mean*SCAL1, min_mean*SCAL1);
+    
     //F1 F2 F3 F4 F5 
-    printf("%lf %lf %lf %lf %lf ", fpeak[1][0], fpeak[2][0], fpeak[3][0], fpeak[4][0], fpeak[5][0]);
+    printf("%lf %lf %lf %lf %lf ",
+    fpeak[1][0]*SCAL2, fpeak[2][0]*SCAL2, fpeak[3][0]*SCAL2, fpeak[4][0]*SCAL2, fpeak[5][0]*SCAL2);
+    
     //A1 A2 A3 A4 A5
-    printf("%lf %lf %lf %lf %lf ", fpeak[1][1], fpeak[2][1], fpeak[3][1], fpeak[4][1], fpeak[5][1]);
+    printf("%lf %lf %lf %lf %lf ",
+    fpeak[1][1]*SCAL3, fpeak[2][1]*SCAL3, fpeak[3][1]*SCAL3, fpeak[4][1]*SCAL3, fpeak[5][1]*SCAL3);
+    
     //FSUM FMEAN FMIN FMAX FMAXMEAN FMINMEAN
-    printf("%lf %lf %lf %lf %lf %lf } ", fsum, fmean, f_min, f_max, fmaxmean, fminmean);
+    printf("%lf %lf %lf %lf %lf %lf } ",
+    fsum*SCAL4, fmean*SCAL4, f_min*SCAL4, f_max*SCAL4, fmaxmean*SCAL4, fminmean*SCAL4);
 
     printf("%s\n", argv[2]);
 
